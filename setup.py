9f3b6bdef163c4b9c133ff0e3d008c9bbbcebdd9
Elessar11777 <elessar11777@gmail.com>
2020-07-07 10:34:25 +0300

    two_plus_two

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
-from distutils.core import setup
-setup(name='Pack_my',
-      version='1.0',
-      py_modules=['Planimetry', 'Stereometry'],
-      )
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Elessar11777",
+    version="0.0.1",
+    author="Denis Garri",
+    author_email="Elessar11777@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Elessar11777/Pack_my.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
